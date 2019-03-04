e0514836008609a229e43f3ca8b24f9606ec8db7
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-03-04 17:32:52 +0100

    Use named route

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -8,6 +8,6 @@ class PagesTest extends TestCase
 {
     public function testWhyPage()
     {
-        $this->get('/why')->assertStatus(200);
+        $this->get(route('why'))->assertStatus(200);
     }
 }
