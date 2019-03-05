09933d8087d22eb4cf743bb5f8d193ec498a4659
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-03-05 11:24:15 +0100

    Work on why page

--- resources/views/why.blade.php
+++ resources/views/why.blade.php
@@ -14,12 +14,25 @@
     <li>Ont-ils besoin d'aide ?</li>
   </ul>
 </p>
+<p>
+  Cet outil répond au besoin d'avoir connaissance des réponses à ces questions pour toutes les équipes.
+</p>
 
-<h2>Pourquoi un rythme hebdomadaire ?</h2>
+<h2>Un partage hebdomadaire</h2>
 <p>
   Lorsque l'on suit la méthodologie agile, il est courant de mettre en place un point d'avancement quotidien, un <i>stand-up</i>. Le fait d'organiser cette mini-réunion quotidienne ne nous parait pas adapté lorsque l'on s'intéresse à d'autres projets que le sien et que l'équipe ne travaille potentiellement pas dans le même lieu. L'organisation d'une telle réunion à heure fixe, de manière physique, en ligne par la voie écrite ou par visioconférence semble trop contraignant par rapport au besoin.
 </p>
 
 <p>Ainsi, un rythme hebdomadaire nous parait plus adapté lorsque l'on s'intéresse aux nouveautés d'autres équipes.</p>
+
+<h2>Un partage asynchrone</h2>
+<p>
+  Réunir à heure fixe plusieurs équipes travaillant sur des projets différents, au sein de diverses structures, est un défi en soit. Ainsi, il a été jugé qu'il était opportun de permettre à chaque équipe de parler de sa semaine, quand elle le souhaite, avant 15h le vendredi.
+</p>
+
+<h2>Une diffusion programmée</h2>
+<p>
+  Si plusieurs équipes présentent leur avancement quand bon leur semble durant la semaine, il est difficile pour les autres équipes de suivre car les informations des autres équipes ne vont pas être disponibles au même moment. Cette charge d'information répartie dans le temps rend l’absorption du contenu laborieux pour tout le monde. Ainsi, nous permettons à chacun de partager leur semaine quand bon leur semble, mais la diffusion de l'ensemble des informations s'effectue au même moment : le vendredi à 15h. Le fait de pouvoir lire tout d'un bloc, au même moment pour tout le monde, facilite l'ingestion de l'information et permet un temps d'échange productif pour les équipes ensuite.
+</p>
 
 @endsection
