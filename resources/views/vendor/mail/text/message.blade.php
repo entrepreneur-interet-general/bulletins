033db47d4f0f9b17d6c7f353af62af3efc07045b
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-03-08 12:05:54 +0100

    Publish default email template

---
+++ resources/views/vendor/mail/text/message.blade.php
@@ -0,0 +1,28 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ config('app.name') }}
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}.
+            @lang('Pour sauver des arbres, mangez des castors et n’imprimez pas cet e-mail.')
+        @endcomponent
+    @endslot
+@endcomponent
