aaf64411253b080f6d8f592be9384a1029329504
Antoine Augusti <antoine.augusti@gmail.com>
2019-03-10 21:04:31 +0000

    Apply fixes from StyleCI

--- app/Notifications/RemindToFillForm.php
+++ app/Notifications/RemindToFillForm.php
@@ -3,8 +3,8 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\SlackMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\SlackMessage;
 
 class RemindToFillForm extends Notification
 {
