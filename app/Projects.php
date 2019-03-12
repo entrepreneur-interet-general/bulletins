cab4282881377b54c55a580296ccc4fcf1f8df22
Antoine Augusti <antoine.augusti@gmail.com>
2019-03-12 17:43:00 +0100

    Apply fixes from StyleCI (#38)

--- app/Projects.php
+++ app/Projects.php
@@ -3,44 +3,42 @@
 namespace App;
 
 use UnexpectedValueException;
-use App\Project;
-use App\Report;
 use Illuminate\Support\Collection;
 
-class Projects extends Collection {
+class Projects extends Collection
+{
+    public function names()
+    {
+        return $this->map->name;
+    }
 
-  public function names()
-  {
-      return $this->map->name;
-  }
+    public function add($item)
+    {
+        if ($this->contains($item->name)) {
+            throw new UnexpectedValueException;
+        }
 
-  public function add($item)
-  {
-      if ($this->contains($item->name)) {
-        throw new UnexpectedValueException;
-      }
+        parent::add($item);
+    }
 
-      parent::add($item);
-  }
+    public function filledProjectsFor($week)
+    {
+        $this->checkIsWeek($week);
 
-  public function filledProjectsFor($week)
-  {
-      $this->checkIsWeek($week);
+        return $this->whereIn('name', Report::where('week_number', $week)->pluck('project'));
+    }
 
-      return $this->whereIn('name', Report::where('week_number', $week)->pluck('project'));
-  }
+    public function unfilledProjectsFor($week)
+    {
+        $this->checkIsWeek($week);
 
-  public function unfilledProjectsFor($week)
-  {
-      $this->checkIsWeek($week);
+        return $this->whereNotIn('name', Report::where('week_number', $week)->pluck('project'));
+    }
 
-      return $this->whereNotIn('name', Report::where('week_number', $week)->pluck('project'));
-  }
-
-  private function checkIsWeek($week)
-  {
-      if (! preg_match('/^\d{4}-\d{2}$/', $week)) {
-        throw new UnexpectedValueException($week. " is not a valid week.");
-      }
-  }
+    private function checkIsWeek($week)
+    {
+        if (! preg_match('/^\d{4}-\d{2}$/', $week)) {
+            throw new UnexpectedValueException($week.' is not a valid week.');
+        }
+    }
 }
