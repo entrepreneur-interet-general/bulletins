ec38f6d426cbf41cde2f685cf5dc9680264387d8
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-03-13 12:24:36 +0100

    Add project logo in email

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -5,7 +5,7 @@ Cette semaine, dans l'équipe.
 
 @foreach ($reports as $report)
 @component('mail::panel')
-## {{ $report->project }}
+## <img src="{{ asset($report->projectObject()->logoUrl) }}" alt="{{ $report->project }}" width="20px"> {{ $report->project }}
 
 - **État d'esprit :** {{ $report->spirit }}
 - **Priorité :** {{ $report->priorities }}
