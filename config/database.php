40ecdfb73be669f9a0eefd49558809f44023c450
Antoine Augusti <antoine.augusti@gmail.com>
2019-03-20 17:41:29 +0100

    Speed up CircleCI build #23
    * Speed up CircleCI build
    
    * Typo

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'strict'         => true,
             'engine'         => null,
             'options'        => array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                // PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]),
         ],
 
