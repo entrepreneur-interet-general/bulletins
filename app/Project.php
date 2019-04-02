a1443b4a0424487dc0803063f03e3f050ed6e2b1
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-02 11:30:14 +0200

    Allow to skip a reminder for a project

--- app/Project.php
+++ app/Project.php
@@ -13,7 +13,7 @@ class Project
 
     public function __construct($name, $notificationChannel, array $members, $logoUrl)
     {
-        if ($notificationChannel !== 'slack') {
+        if ($notificationChannel !== 'slack' and ! is_null($notificationChannel)) {
             throw new UnexpectedValueException;
         }
 
@@ -25,6 +25,10 @@ class Project
 
     public function notify()
     {
+        if (is_null($notificationChannel)) {
+            return;
+        }
+
         $format = "Pour le moment nous n'avons pas de nouvelles du défi %s :sob: Votre mission : partager les nouvelles de la semaine à la promo en moins d'une heure. Attention, décollage à 15h ! :rocket: :point_right: %s";
         $text = sprintf($format, $this->name, config('app.url'));
 
