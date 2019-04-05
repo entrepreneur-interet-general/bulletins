055dcecfaf3c2de44a4840d3402ccc5f5b7222a0
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-05 16:23:29 +0200

    Add Bugsnag error handling

--- config/logging.php
+++ config/logging.php
@@ -36,8 +36,12 @@ return [
     'channels' => [
         'stack' => [
             'driver'            => 'stack',
-            'channels'          => ['daily'],
+            'channels'          => ['single', 'bugsnag'],
             'ignore_exceptions' => false,
+        ],
+
+        'bugsnag' => [
+            'driver' => 'bugsnag',
         ],
 
         'single' => [
