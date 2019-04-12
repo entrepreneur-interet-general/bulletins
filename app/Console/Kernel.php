698ea62361a9d07b1253a7f09816fe55fa7bd3e5
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-12 11:01:44 +0200

    Add 2nd reminder at 14:45

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,6 +39,11 @@ class Kernel extends ConsoleKernel
         })->timezone(config('app.report_timezone'))->fridays()->at('14:00');
 
         $schedule->call(function () {
+            $currentWeek = now()->format('Y-W');
+            config('app.projects')->unfilledProjectsFor($currentWeek)->map->notify();
+        })->timezone(config('app.report_timezone'))->fridays()->at('14:45');
+
+        $schedule->call(function () {
             Mail::to(config('app.report_email'))->send(new WeeklyReport);
         })->timezone(config('app.report_timezone'))->fridays()->at('15:05');
     }
