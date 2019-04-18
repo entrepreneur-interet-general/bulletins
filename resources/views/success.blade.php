2419f861337eb482ff86167ee500bff9543873d1
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-18 11:21:14 +0200

    Localisation for success

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,10 +1,10 @@
 @extends('master')
 
 @section('content')
-<h1>Bilan de la semaine {{ $week }}</h1>
-<div class="notification success">Tout est bien enregistré !</div>
+<h1>{{ __('form.retrospective_title', compact('week')) }}</h1>
+<div class="notification success">{{ __('success.saved') }}</div>
 
-<p>Merci d'avoir pris quelques minutes pour nous dire ce qu'il s'est passé cette semaine ! Passe une bonne journée.</p>
+<p>{{ __('success.thanks') }}</p>
 
 <iframe src="https://giphy.com/embed/{{ $gif->id }}" width="{{ $gif->width }}" height="{{ $gif->height }}" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>
 @endsection
