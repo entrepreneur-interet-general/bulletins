37fa2efcf239e6cdbe7b2a90973a88da8e93ab07
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-18 12:16:01 +0200

    Localisation for emails/report

---
+++ resources/lang/fr/emails.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'intro' => "Cette semaine, dans l'équipe.",
+    'help_requests' => "Demandes d'aide",
+    'news' => 'Nouvelles de la semaine',
+    'mood' => "État d'esprit :",
+    'priority' => 'Priorité :',
+    'ups_and_downs' => 'Victoire / Difficulté :',
+    'help' => 'Besoin :',
+    'no_news' => "Malheureusement, nous n'avons pas de nouvelles pour ces projets : :projects 😢.",
+    'all_filled' => 'Tout le monde a rempli son bilan ! 💪 Merci 🙏',
+    'outro' => 'Passez un bon week-end ! 🏝',
+];
