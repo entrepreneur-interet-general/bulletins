665267f94f6cb0631430d734937f4d74a436c8fe
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-19 16:48:25 +0200

    Switch to trans in views

--- resources/views/emails/report.blade.php
+++ resources/views/emails/report.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-# {{ __('form.retrospective_title', ['week' => $weekNumber]) }}
-{{ __('emails.intro') }}
+# {{ trans('form.retrospective_title', ['week' => $weekNumber]) }}
+{{ trans('emails.intro') }}
 
 @if($helpRequests->count() > 0)
-## {{ __('emails.help_requests') }}
+## {{ trans('emails.help_requests') }}
 @component('mail::panel')
 @foreach($helpRequests as $project => $help)
 - **{{ $project }} :** {{ $help }}
@@ -12,26 +12,26 @@
 @endcomponent
 @endif
 
-## {{ __('emails.news') }}
+## {{ trans('emails.news') }}
 @foreach ($reports as $report)
 @component('mail::panel')
 ## <img src="{{ asset($report->projectObject()->logoUrl) }}" alt="{{ $report->project }}" width="20"> {{ $report->project }}
 
-- **{{ __('emails.mood') }}** {{ $report->spirit }}
-- **{{ __('emails.priority') }}** {{ $report->priorities }}
-- **{{ __('emails.ups_and_downs') }}** {{ $report->victories }}
+- **{{ trans('emails.mood') }}** {{ $report->spirit }}
+- **{{ trans('emails.priority') }}** {{ $report->priorities }}
+- **{{ trans('emails.ups_and_downs') }}** {{ $report->victories }}
 @if (isset($report->help))
-- **{{ __('emails.help') }}** {{ $report->help }}
+- **{{ trans('emails.help') }}** {{ $report->help }}
 @endif
 @endcomponent
 @endforeach
 
 @if ($projectsNoInfo->count() > 0)
-{{ __('emails.no_news', ['projects' => $projectsNoInfo->implode(', ')]) }}
+{{ trans('emails.no_news', ['projects' => $projectsNoInfo->implode(', ')]) }}
 @else
-{{ __('emails.all_filled') }}
+{{ trans('emails.all_filled') }}
 @endif
 
-{{ __('emails.outro') }}
+{{ trans('emails.outro') }}
 
 @endcomponent
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 @if ($canBeFilled and ! $allFilled)
   <form action="{{ route('reports.store') }}" method="post" name="form">
     @csrf
-    <h1>{{ __('form.retrospective_title', compact('week')) }}</h1>
-    <p>{{ __('form.description') }}</p>
+    <h1>{{ trans('form.retrospective_title', compact('week')) }}</h1>
+    <p>{{ trans('form.description') }}</p>
 
     <div class="form__group">
       <div class="text-quote"><p>@lang('form.previous_bulletins.week')</p></div>
@@ -20,7 +20,7 @@
     @endif
 
     <div class="form__group">
-      <label for="project">{{ __('form.project') }}</label>
+      <label for="project">{{ trans('form.project') }}</label>
       <select name="project">
         @foreach($projects->map->name as $project)
         <option
@@ -36,7 +36,7 @@
 
     <div class="form__group">
       <fieldset>
-        <legend>{{ __('form.team_mood') }}</legend>
+        <legend>{{ trans('form.team_mood') }}</legend>
         <input type="radio" name="spirit" id="spirit" value="☹️"><label for="spirit" class="label-inline">☹️</label>
         <input type="radio" name="spirit" id="spirit" value="😐"><label for="spirit" class="label-inline">😐</label>
         <input type="radio" name="spirit" id="spirit" value="🙂"><label for="spirit" class="label-inline">🙂</label>
@@ -45,28 +45,28 @@
     </div>
 
     <div class="form__group">
-      <label for="priorities">{{ __('form.priority') }}</label>
-      <textarea name="priorities" rows="3" placeholder="{{ __('form.priority.placeholder') }}" required maxlength="300">{{ old('priorities') }}</textarea>
+      <label for="priorities">{{ trans('form.priority') }}</label>
+      <textarea name="priorities" rows="3" placeholder="{{ trans('form.priority.placeholder') }}" required maxlength="300">{{ old('priorities') }}</textarea>
     </div>
 
     <div class="form__group">
-      <label for="victories">{{ __('form.ups_and_downs') }}</label>
-      <textarea name="victories" rows="3" placeholder="{{ __('form.ups_and_downs.placeholder') }}" required maxlength="300">{{ old('victories') }}</textarea>
+      <label for="victories">{{ trans('form.ups_and_downs') }}</label>
+      <textarea name="victories" rows="3" placeholder="{{ trans('form.ups_and_downs.placeholder') }}" required maxlength="300">{{ old('victories') }}</textarea>
     </div>
 
     <div class="form__group">
-      <label for="help">{{ __('form.help') }}<span class="label">{{ __('form.optional') }}</span></label>
-      <textarea name="help" rows="3" placeholder="{{ __('form.help.placeholder') }}" maxlength="300">{{ old('help') }}</textarea>
+      <label for="help">{{ trans('form.help') }}<span class="label">{{ trans('form.optional') }}</span></label>
+      <textarea name="help" rows="3" placeholder="{{ trans('form.help.placeholder') }}" maxlength="300">{{ old('help') }}</textarea>
     </div>
 
     <div class="form__group">
-      <button class="button" type="submit" name="validate">{{ __('form.save') }}</button>
+      <button class="button" type="submit" name="validate">{{ trans('form.save') }}</button>
     </div>
   </form>
 @else
-  <h1>{{ __('form.retrospective_title', compact('week')) }}</h1>
+  <h1>{{ trans('form.retrospective_title', compact('week')) }}</h1>
   <p>
-    {{ __('form.cant_fill') }}
+    {{ trans('form.cant_fill') }}
   </p>
 @endif
 @endsection
