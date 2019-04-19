c3fa4643e64625d3cf3a7156a7601985663914a2
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-19 16:48:25 +0200

    Switch to trans in views

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,10 +1,10 @@
 @extends('master')
 
 @section('content')
-<h1>{{ __('form.retrospective_title', compact('week')) }}</h1>
-<div class="notification success">{{ __('success.saved') }}</div>
+<h1>{{ trans('form.retrospective_title', compact('week')) }}</h1>
+<div class="notification success">{{ trans('success.saved') }}</div>
 
-<p>{{ __('success.thanks') }}</p>
+<p>{{ trans('success.thanks') }}</p>
 
 <iframe src="https://giphy.com/embed/{{ $gif->id }}" width="{{ $gif->width }}" height="{{ $gif->height }}" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></p>
 @endsection
