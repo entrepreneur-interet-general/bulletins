2c143b5d00c388743614e9d4dfd7fe35f439907b
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-19 18:28:56 +0200

    Generate empty English files

---
+++ resources/lang/en/emails.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'intro' => '',
+    'help_requests' => '',
+    'news' => '',
+    'mood' => '',
+    'priority' => '',
+    'ups_and_downs' => '',
+    'help' => '',
+    'no_news' => '',
+    'all_filled' => '',
+    'outro' => '',
+    'subject' => '',
+];
