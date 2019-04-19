c66fc0e44ffe718e8a756891f7b52f4d57352f9f
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-19 18:28:56 +0200

    Generate empty English files

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'previous_bulletins' => '',
+    'about' => '',
+    'source_code' => '',
+    'hero_title' => '',
+    'hero_subtitle' => '',
+];
---
+++ resources/lang/en/reports.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return [
+    'by_month' => '',
+    'actions' => '',
+    'share' => '',
+    'csv_export' => '',
+    'time_period' => '',
+    'mood' => '',
+    'priority' => '',
+    'ups_and_downs' => '',
+    'help' => '',
+    'share_title' => '',
+    'share_description' => '',
+    'copy' => '',
+    'close' => '',
+    'previous_reports' => '',
+];
