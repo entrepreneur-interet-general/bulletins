3c8715f516302acaa34623a92a64d6b784a9e8be
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-19 18:28:46 +0200

    Put notification translations in right file

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'general_reminder' => "<!here|here> :wave: Hello ! Bonne nouvelle, c'est bientôt la fin de la semaine ! :tada: C'est le moment de partager nos réussites et nos besoins à la promo (attention, l'e-mail part à 15h :rocket:) :point_right: :url",
-    'individual_reminder' => "Pour le moment nous n'avons pas de nouvelles du projet :project :sob: Votre mission : partager les nouvelles de la semaine à la promo en moins d'une heure. Attention, décollage à 15h ! :rocket: :point_right: :url",
+    'general_reminder' => '',
+    'individual_reminder' => '',
 ];
