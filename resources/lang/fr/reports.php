b1f100294e0a7c1dfba0156b0486534631ae4901
Antoine Augusti <antoine.augusti@gmail.com>
2019-04-19 16:45:08 +0200

    Apply fixes from StyleCI (#66)

--- resources/lang/fr/reports.php
+++ resources/lang/fr/reports.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    'by_month' => "Bilans par mois",
-    'actions' => "Actions",
-    'share' => "Partager",
-    'csv_export' => "Exporter en CSV",
-    'time_period' => "Du :start au :end",
+    'by_month' => 'Bilans par mois',
+    'actions' => 'Actions',
+    'share' => 'Partager',
+    'csv_export' => 'Exporter en CSV',
+    'time_period' => 'Du :start au :end',
     'mood' => "État d'esprit :",
     'priority' => 'Priorité :',
     'ups_and_downs' => 'Hauts et bas :',
     'help' => "Demande d'aide :",
     'share_title' => "Partager l'historique des bilans",
     'share_description' => "Pour partager <b>uniquement</b> l'historique des bilans du projet :project, vous pouvez partager ce lien unique.",
-    'copy' => "Copier",
-    'close' => "Fermer",
+    'copy' => 'Copier',
+    'close' => 'Fermer',
 ];
