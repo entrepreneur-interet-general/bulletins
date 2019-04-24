3fd275c48034c969768679223ae3de2435459482
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-24 13:59:19 +0200

    More translation

--- resources/lang/en/success.php
+++ resources/lang/en/success.php
@@ -2,6 +2,6 @@
 
 
 return [
-    'saved' => '',
-    'thanks' => '',
+    'saved' => 'Your bulletin has been saved_',
+    'thanks' => 'Thanks for taking the time to tell us how went your week! Have a nice day!',
 ];
