b5ed4cff4d188286ffdce6bdcb8ec03c09eb39be
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-24 14:10:09 +0200

    Improve translation on form

--- resources/lang/en/form.php
+++ resources/lang/en/form.php
@@ -3,7 +3,7 @@
 return [
     'retrospective_title' => 'Bulletins of the week :week',
     'description' => 'Take a look back at this week. 5 minutes, 300 characters per field — a bit more than a tweet.',
-    'previous_bulletins.week' => 'Your answers will be shared to the team on <b>Friday at 3PM.</b>',
+    'previous_bulletins.week' => 'Your answers will be shared by email to the team on <b>Friday at 3PM.</b>',
     'project' => 'Your project',
     'already_filled' => '(already filled)',
     'team_mood' => 'Your team mood',
