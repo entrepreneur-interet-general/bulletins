a9336b5bcc362e63de175c6e21cbe9c3a695ba09
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-04-26 12:25:27 +0200

    Add database seeder #73
    * Write database seeder
    
    * Apply fixes from StyleCI (#72)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,37 @@
 <?php
 
+use App\Report;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = FakerFactory::create();
+
+        foreach (config('app.projects')->names() as $name) {
+            foreach ($this->weeks() as $week) {
+                if ($faker->numberBetween(1, 100) <= 80) {
+                    factory(Report::class)->create([
+                        'project' => $name,
+                        'week_number' => $week,
+                    ]);
+                }
+            }
+        }
+    }
+
+    private function weeks()
+    {
+        $thisFriday = new Carbon('friday');
+        $weeks = [$thisFriday->format('Y-W')];
+
+        foreach (range(1, 30) as $weekSub) {
+            $weeks[] = $thisFriday->copy()->subWeek($weekSub)->format('Y-W');
+        }
+
+        return $weeks;
     }
 }
