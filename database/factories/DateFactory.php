52134b072f0096eab80ea0078267657190668167
Antoine Augusti <antoine.augusti@gmail.com>
2019-05-26 14:31:57 +0200

    Apply fixes from StyleCI (#84)

--- database/factories/DateFactory.php
+++ database/factories/DateFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Date::class, function (Faker $faker) {
     return [
-        'project' => $faker->randomElement(config('app.projects')->names()),
-        'date' => $faker->dateTimeThisYear(now()->addYear(1))->format('Y-m-d'),
+        'project'     => $faker->randomElement(config('app.projects')->names()),
+        'date'        => $faker->dateTimeThisYear(now()->addYear(1))->format('Y-m-d'),
         'description' => $faker->text(100),
     ];
 });
