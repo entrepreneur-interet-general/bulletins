ef74d074358924528debeca021b8f65eb72708db
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-06-02 19:02:48 +0200

    Add test for WeeklyReport

---
+++ tests/Feature/WeeklyReportTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Mail\WeeklyReport;
+use App\Date;
+use App\Report;
+use App\Projects;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use UnexpectedValueException;
+
+class WeeklyReportTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testInvalidWeek()
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        new WeeklyReport('foo');
+    }
+
+    public function testForWeek()
+    {
+        $week = '2019-20';
+        $names = config('app.projects')->names();
+
+        factory(Report::class)->create(['week_number' => $week, 'project' => $names[0]]);
+        factory(Report::class)->create(['week_number' => $week, 'project' => $names[1]]);
+        factory(Report::class)->create(['week_number' => '2018-01']);
+
+        factory(Date::class)->create(['date' => '2019-05-17']);
+        factory(Date::class)->create(['date' => '2019-05-24']);
+        factory(Date::class)->create(['date' => '2019-05-25']);
+
+        $view = (new WeeklyReport($week))->build();
+
+        $this->assertEquals($view->subject, 'Bulettins of the week 2019-20');
+
+        $this->assertEquals($view->viewData['weekNumber'], $week);
+        $this->assertEquals($view->viewData['reports']->pluck('id')->sort()->values(), Report::forWeek($week)->pluck('id')->sort()->values());
+        $this->assertEquals($view->viewData['upcomingDates']->pluck('id'), Date::where('date', '2019-05-24')->pluck('id'));
+    }
+}
