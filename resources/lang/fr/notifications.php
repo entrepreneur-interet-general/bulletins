b3bb5bd8877d7543960c347ea61f22212fb322af
Antoine Augusti <antoine.augusti@gmail.com>
2019-06-17 16:44:18 +0200

    Apply fixes from StyleCI (#96)

--- resources/lang/fr/notifications.php
+++ resources/lang/fr/notifications.php
@@ -4,5 +4,5 @@ return [
     'general_reminder' => "<!here|here> :wave: Hello ! Bonne nouvelle, c'est bientôt la fin de la semaine ! :tada: C'est le moment de partager nos réussites et nos besoins à la promo (attention, l'e-mail part à 15h :rocket:) :point_right: :url",
     'individual_reminder' => "Pour le moment nous n'avons pas de nouvelles du projet :project :sob: Votre mission : partager les nouvelles de la semaine à la promo en moins d'une heure. Attention, décollage à 15h ! :rocket: :point_right: :url",
     'individual_reminder_mail' => "Pour le moment nous n'avons pas de nouvelles du projet :project. Votre mission : partager les nouvelles de la semaine aux autres en moins d'une heure. Attention, décollage à 15h !",
-    'individual_reminder_mail.button' => "Remplir ma rétrospective",
+    'individual_reminder_mail.button' => 'Remplir ma rétrospective',
 ];
