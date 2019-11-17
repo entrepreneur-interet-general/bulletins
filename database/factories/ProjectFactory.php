6cba113ac5096d5e44e6f5945d3c9b76d1ff1a63
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-11-17 17:51:48 +0100

    Add factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Project;
+use Faker\Generator as Faker;
+use Illuminate\Support\Carbon;
+
+$factory->define(Project::class, function (Faker $faker) {
+    return [
+        'name' => 'The name',
+        'channel' => 'slack',
+        'members' => [],
+        'logoUrl' => 'img.jpg',
+        'endsOn' => Carbon::tomorrow(),
+    ];
+});
+
+$factory->state(Project::class, 'inactive', [
+    'endsOn' => Carbon::yesterday(),
+]);
