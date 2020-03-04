0ad4f178c4e9c94a25c96d5a2c7fa6729866aefa
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-03-04 09:30:30 -0500

    Upgrade to Laravel 7 (#140)
    * Upgrade to Laravel 7
    
    * Apply fixes from StyleCI (#139)
    
    * Bump Laravel

--- config/session.php
+++ config/session.php
@@ -166,7 +166,9 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
+
+    'same_site' => 'lax',
 
     /*
     |--------------------------------------------------------------------------
