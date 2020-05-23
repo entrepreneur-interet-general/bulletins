24ced5914567847268ea295505396321f98adeca
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-05-23 10:36:33 -0400

    Security: set Referrer-Policy HTTP header (#153)

---
+++ app/Http/Middleware/ReferrerPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class ReferrerPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (get_class($response) != StreamedResponse::class) {
+            $response->header('Referrer-Policy', 'no-referrer');
+        }
+
+        return $response;
+    }
+}
